687bbec503351907a7ed993fd96a615ddf3821ad
nicholasMeyers <s218019@student.aie.edu>
2022-02-14 12:53:33 -0600

    Fixed up Smaller Inconsistencies and Errors

--- DictionaryPractice/DictionaryPractice.cpp
+++ DictionaryPractice/DictionaryPractice.cpp
@@ -5,6 +5,7 @@ int main()
 {
     Dictionary<char, int> dictionary = Dictionary<char, int>();
     dictionary.clear();
+    dictionary.addItem('c', 6);
     dictionary.addItem('a', 3);
     dictionary.remove('a');
     dictionary.remove('a');
@@ -12,5 +13,4 @@ int main()
     int value = 0;
     dictionary.remove('a', value);
     std::cout << value << std::endl;
-
 }
